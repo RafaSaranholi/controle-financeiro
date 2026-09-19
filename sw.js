e9ffcb7db299aa9d797bc20afa80392d7b9694ff
RafaSaranholi <rafael.saranholi@gmail.com>
2026-09-19 20:24:12 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'controle-financeiro-v1';
+const APP_SHELL = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  event.respondWith(
+    caches.match(req).then(cached => cached || fetch(req).then(res => {
+      if (req.url.startsWith(self.location.origin)) {
+        const clone = res.clone();
+        caches.open(CACHE).then(cache => cache.put(req, clone));
+      }
+      return res;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
