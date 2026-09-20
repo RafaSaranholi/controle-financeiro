fa2c18341e03249b196b989519085da913b03971
RafaSaranholi <rafael.saranholi@gmail.com>
2026-09-20 19:13:00 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'controle-financeiro-v4-online-auto-update';
+const CACHE_NAME = 'controle-financeiro-v5';
 const APP_SHELL = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install', event => {
